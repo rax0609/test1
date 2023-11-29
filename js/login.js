3639713af31a58fcee520a67c4e7af5ab02610d9
rax0609 <91789830+rax0609@users.noreply.github.com>
2023-11-30 03:31:38 +0800

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,4 +1,5 @@
 let sendButton = document.querySelector('button');
+const axios = require('axios');
 
 const showAlert = () => {
   Swal.fire({
@@ -10,9 +11,35 @@ const showAlert = () => {
       //window.location.href = 'http://60.249.215.114/stust_student/admin/index.php'
       const CLIENT_ID = '893799094533894144';
       const CLIENT_SECRET = 'HZopOlif6SHJsb148Yk0zGX8_DBfDM0x';
-      const REDIRECT_URI = 'https://test.rxteam.net/test.html';
+      const REDIRECT_URI = '';
       window.location.href = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify`
       const code = req.query.code;
+      const params = new URLSearchParams({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code,
+        grant_type: 'authorization_code',
+        redirect_uri: REDIRECT_URI,
+        scope: 'identify guilds'
+      });
+    
+      const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'application/x-www-form-urlencoded'
+      };
+    
+      const response = await axios.post('https://discord.com/api/oauth2/token', params, headers);
+    
+      const userResponse = await axios.get('https://discord.com/api/users/@me', {
+        headers: {
+          Authorization: `Bearer ${response.data.access_token}`
+        }
+      });
+    
+      //'https://discord.com/api/oauth2/token'
+      
+    
+      res.send(userResponse.data);
     }
   })
 }
